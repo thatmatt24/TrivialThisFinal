b42641e8164f69603f537419921c18830d57a52c
thatmatt24 <36048251+thatmatt24@users.noreply.github.com>
2018-08-21 16:51:20 +0000

    Create Player2_Hinter.kt

---
+++ app/src/main/java/com/example/matt/trivialthisfinal/Player2_Hinter.kt
@@ -0,0 +1,15 @@
+package com.example.matt.trivialthisfinal
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.player2_activity.*
+
+
+class Player2_Hinter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.player2_activity)
+    }
+
+}
